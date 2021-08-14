1e419570a105ae62b55054aafe5e40891c8c5915
Ehtisham Rauf <ehtishamrauf492@gmail.com>
2021-08-14 13:32:35 +0500

    Initial Commit

---
+++ AfinitiTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AfinitiTask
+//
+//  Created by Ehtisham Rauf on 14/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
