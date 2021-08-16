ae307f506c257ad8d93ee91514fc6bfa98f4cf0a
Ehtisham Rauf <ehtishamrauf492@gmail.com>
2021-08-16 23:03:00 +0500

    1 Task Completed

--- AfinitiTask/Controllers/MultiThreadingViewController.swift
+++ AfinitiTask/Controllers/MultiThreadingViewController.swift
@@ -6,13 +6,87 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class MultiThreadingViewController: UIViewController {
 
+    //MARK: - OUTLETS -
+    @IBOutlet var imageViewCollection: [UIImageView]!
+    @IBOutlet var progressBarCollection: [UIProgressView]!
+    
+    //MARK: - PROPERTIES -
+    let imagesUrl : [String] =
+        [
+            "https://wonderfulengineering.com/wp-content/uploads/2013/12/high-definition-wallpaper-3.jpg",
+            "https://www.desktopbackground.org/download/o/2012/12/09/496563_38-beautiful-wallpapers-in-high-resolution-for-free-download_1920x1080_h.jpg",
+            "https://cdn.pixabay.com/photo/2018/09/23/18/30/drop-3698073__480.jpg"
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //MARK: - IBACTIONS -
+    @IBAction func downloadClickHandler(_ sender: UIButton) {
+        
+        self.loadImagesConcurrently()
+        
+    }
+    
+    
 }
 
+//MARK: - IMAGE HANDLERS -
+extension MultiThreadingViewController{
+    
+    //MARK: - IMAGE LOADING HANDLER -
+    func loadImagesConcurrently(){
+        
+        let dispatchQueue = DispatchQueue(label: "concurrent.queue",attributes: .concurrent)
+        
+        for (index,imageURL) in self.imagesUrl.enumerated(){
+            
+            let progressBar = self.progressBarCollection[index]
+            let imageView = self.imageViewCollection[index]
+            
+            dispatchQueue.async {
+
+                self.downloadImage(imageUrl: imageURL, progressBar: progressBar, imageView: imageView)
+            
+            }
+           
+        }
+        
+    }
+    
+    //MARK: - IMAGE DOWNLOADING HANDLER -
+    func downloadImage(imageUrl:String,progressBar:UIProgressView,imageView:UIImageView){
+        
+        AF.request(imageUrl)
+            .downloadProgress { progress in
+                
+                DispatchQueue.main.async {
+                    
+                    progressBar.progress = Float(progress.fractionCompleted)
+                    
+                }
+                
+            }
+            .responseData { response in
+                
+                DispatchQueue.main.async {
+                    
+                    if let DownloadedImage = UIImage(data: response.data!){
+                        
+                        imageView.image = DownloadedImage
+                        
+                    }
+                    
+                }
+                
+            }
+        
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AfinitiTask' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AfinitiTask
+  pod 'Alamofire', '~> 5.2'
+  pod 'Socket.swift', '~> 2.4.0'
+
+
+  target 'AfinitiTaskTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AfinitiTaskUITests' do
+    # Pods for testing
+  end
+
+end
